86715b81dea3c22e18f87797c36319d72ccc348c
rdmarqueziv4 <rdmarqu2@uci.edu>
2025-11-03 18:40:39 -0800

    PlayerClawAttack
    Player is able to melee attack when within range of enemy

---
+++ MetroidvaniaIntroVGDC/Assets/Scripts/PlayerClawAttack.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerClawAttack : MonoBehaviour
+{
+    public InputSystem_Actions actions;
+    public Transform hitboxTransform;
+    public float hitboxRadius = 0.5f;
+    public LayerMask hitLayers;
+
+    public float damage;
+
+    bool isHitting;
+
+    Rigidbody2D rb;
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+        actions.Player.Enable();
+    }
+    void OnEnable()
+    {
+        actions.Player.Attack.performed += OnClawAttack;
+        actions.Player.Attack.canceled += OnClawAttack;
+    }
+    void OnDisable()
+    {
+        actions.Player.Attack.performed -= OnClawAttack;
+        actions.Player.Attack.canceled -= OnClawAttack;
+    }
+    private void OnClawAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (isHitting)
+            {
+                Debug.Log("Claw Attack Hit!");
+                // Implement logic for when the claw attack hits an enemy
+            }
+            else
+            {
+                Debug.Log("Claw Attack Missed!");
+                // Implement logic for when the claw attack misses
+            }
+        }
+        else if (context.canceled)
+        {
+            Debug.Log("Claw Attack Canceled");
+            // Implement logic for when the claw attack is canceled, if needed
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHitting = Physics2D.OverlapCircle(
+            hitboxTransform.position, hitboxRadius, hitLayers);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.purple;
+        Gizmos.DrawWireSphere(hitboxTransform.position, hitboxRadius);
+    }
+}
