f24b9897f30532e32408d03504b7e6ba91859652
ethan <122701755+army-ofme@users.noreply.github.com>
2025-11-09 17:09:24 -0800

    added enemies to the map

--- MetroidvaniaIntroVGDC/Assets/Scripts/PatrolEnemy/MeleeEnemy.cs
+++ MetroidvaniaIntroVGDC/Assets/Scripts/PatrolEnemy/MeleeEnemy.cs
@@ -78,11 +78,11 @@ public class meleeEnemy : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0f, Vector2.left, 0.1f, playerlayer);
-        if (hit.collider != null)
+        if (hit.collider.CompareTag("Player"))
         {
             playerhealth = hit.transform.GetComponent<Health>();
         }
-        return hit.collider != null;
+        return hit.collider.CompareTag("Player");
     }
 
     private void OnDrawGizmos()
