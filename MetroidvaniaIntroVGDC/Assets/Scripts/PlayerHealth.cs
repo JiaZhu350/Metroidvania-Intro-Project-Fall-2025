0c34cb1e5f41e20f0285800571b96aad336292c5
Anthony Melgar <111323264+CodingFire87@users.noreply.github.com>
2025-11-11 21:00:07 -0800

    Player Healing implemented properly

--- MetroidvaniaIntroVGDC/Assets/Scripts/PlayerHealth.cs
+++ MetroidvaniaIntroVGDC/Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth;  // made public -Bryce
     private Animator anim;
+    public InputSystem_Actions actions;
     private bool dead = false;
+    public float _heal = 1f;
+    public int healthItems = 3;
 
     public GameObject HPUI;  // -Bryce
     private void updateUI(){HPUI.GetComponent<HP_uiHandler>().HealthChanged();}  // -Bryce
@@ -15,6 +19,20 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = startingHealth - 2;
         Debug.Log("Player health: " + currentHealth);
+        actions = new InputSystem_Actions();
+        actions.Player.Enable();
+    }
+
+    private void OnEnable()
+    {
+        actions.Player.Heal.performed += Heal;
+        actions.Player.Heal.canceled += Heal;
+    }
+
+    void OnDisable()
+    {
+        actions.Player.Attack.performed -= Heal;
+        actions.Player.Attack.canceled -= Heal;
     }
 
     // Update is called once per frame
@@ -41,8 +59,21 @@ public class PlayerHealth : MonoBehaviour
 
     public void HealthItem()
     {
-        currentHealth = Mathf.Clamp(currentHealth + 20, 0, startingHealth);
-        Debug.Log("Player health increased: " + currentHealth);
-        updateUI();  // -Bryce
+        healthItems++;
+    }
+
+    public void Heal(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Heal button pressed");
+            if (healthItems > 0 && currentHealth < startingHealth && !dead)
+            {
+                healthItems--;
+                currentHealth = Mathf.Clamp(currentHealth + _heal, 0, startingHealth);
+                Debug.Log("Player healed: " + currentHealth);
+                updateUI();  // -Bryce
+            }
+        }
     }
 }
