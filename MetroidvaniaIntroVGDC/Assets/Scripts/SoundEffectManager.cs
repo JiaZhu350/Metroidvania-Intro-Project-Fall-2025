7751cd2c804a03d34db26da532c79c9075a5ea5c
Anthony Melgar <111323264+CodingFire87@users.noreply.github.com>
2025-11-21 00:40:04 -0800

    Sound Effect skeleton code added

--- MetroidvaniaIntroVGDC/Assets/Scripts/SoundEffectManager.cs
+++ MetroidvaniaIntroVGDC/Assets/Scripts/SoundEffectManager.cs
@@ -6,6 +6,8 @@ public class SoundEffectManager : MonoBehaviour
 {
     public static SoundEffectManager Instance;
     [SerializeField] private AudioSource soundFXPObject;
+    [SerializeField] private AudioSource MusicSource;
+    public AudioClip backgroundMusicClip;
 
     private void Awake()
     {
@@ -20,6 +22,13 @@ public class SoundEffectManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        MusicSource.clip = backgroundMusicClip;
+        MusicSource.loop = true;
+        MusicSource.Play();
+    }
+
     public void PlaySoundFXClip(AudioClip clip, Transform spawnTransform, float volume = 1.0f)
     {
         AudioSource audioSource = Instantiate(soundFXPObject, spawnTransform.position, Quaternion.identity);
@@ -28,6 +37,19 @@ public class SoundEffectManager : MonoBehaviour
         audioSource.Play();
 
         float clipLength = clip.length;
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayRandomSoundFXClip(AudioClip[] clip, Transform spawnTransform, float volume = 1.0f)
+    {
+        int Rand = Random.Range(0, clip.Length);
+
+        AudioSource audioSource = Instantiate(soundFXPObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = clip[Rand];
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        float clipLength = clip[Rand].length;
         Destroy(audioSource.gameObject, clipLength);
     }
 }
