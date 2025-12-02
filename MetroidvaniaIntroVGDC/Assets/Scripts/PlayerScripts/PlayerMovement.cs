925c7db087f710e5f58159108577f4a9526a99ad
Anthony Melgar <111323264+CodingFire87@users.noreply.github.com>
2025-12-02 10:50:15 -0800

    Music added

--- MetroidvaniaIntroVGDC/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ MetroidvaniaIntroVGDC/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,6 +1,6 @@
 
 using System;
-using Unity.VisualScripting.ReorderableList;
+//using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
